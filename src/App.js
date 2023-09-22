4f7de55fc0b049c8e4860a72ad31343489b92b34
humbertic <pugarosabal@gmail.com>
2023-09-22 09:30:09 -0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import Header from "./components/Header"
+import Meme from "./components/Meme"
+export default function App () {
+  return (
+    <div>
+      <Header/>
+      <Meme/>
+
+    </div>
+  )
+}
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <img className="header--image" src="../images/Troll-Face.png" />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4>React Course - Project 3</h4>
+        </header>
+
+    )
+}
